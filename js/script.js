8f68dc1ec05190b3adfce27a1bc17c9901115180
karen <karensandovalg.e@gmail.com>
2023-01-21 18:46:53 -0300

    Modificacion

--- js/script.js
+++ js/script.js
@@ -11,14 +11,9 @@ nombre = prompt("Ingresar Nombre")
 
 if (nombre == "") {
     alert("NO INGRESASTE NINGUN VALOR")
-}else{
-    alert("Nombre Ingresado:"+" "+nombre)
-} 
-<<<<<<< HEAD
-
-=======
-.
->>>>>>> 6f294e953e7263090a1030374c4f2154d6e70494
+} else {
+    alert("Nombre Ingresado:" + " " + nombre)
+}
 
 console.log("Nombre Ingresado:" + " " + nombre)
 
@@ -27,52 +22,47 @@ apellido = prompt("Ingresar Apellido")
 
 if (apellido == "") {
     alert("NO INGRESASTE NINGUN VALOR")
-}else{
-    alert("Apellido Ingresado:"+" "+ apellido)
+} else {
+    alert("Apellido Ingresado:" + " " + apellido)
 }
 console.log("Apellido Ingresado:" + " " + apellido)
 
 
-edad = Number (prompt("Ingresar edad"))
-if(edad < 18){
+edad = Number(prompt("Ingresar edad"))
+if (edad < 18) {
     alert("Para acceder al sistema de turnos debe ser mayor de 18 años")
 }
-    else if (edad > 60){
-        alert("Para acceder al sistema de turnos debes ser menor de 60 años")
-    }else{
-    console.log("Edad Ingresada:"+" "+edad)
+else if (edad > 60) {
+    alert("Para acceder al sistema de turnos debes ser menor de 60 años")
+} else {
+    console.log("Edad Ingresada:" + " " + edad)
 }
 
 
-correo = prompt ("Ingresar E-mail")
+correo = prompt("Ingresar E-mail")
 if (correo == "") {
     alert("NO INGRESASTE NINGUN VALOR")
-}else{
-    alert("E-mail Ingresado:"+" "+correo)
+} else {
+    alert("E-mail Ingresado:" + " " + correo)
 }
-console.log("E-mail ingresado:"+" "+ correo)
+console.log("E-mail ingresado:" + " " + correo)
 
 
 
 for (let turno = 1; turno <= 1; turno++) {
     let obraSocial = prompt("Ingresa Obra Social");
-    if (obraSocial == "ESC"){
+    if (obraSocial == "ESC") {
         break
     }
-    alert(" Turno  N° "+turno+" Nombre: "+ nombre +" "+ apellido);
+    alert(" Turno  N° " + turno + " Nombre: " + nombre + " " + apellido);
 }
-<<<<<<< HEAD
+
 
 function aviso(direccion) {
     let saludo2 = "con DNI y numero de turno."
-    alert(direccion +" "+ saludo2 + " " + "¡¡Muchas gracias!!")
-    
+    alert(direccion + " " + saludo2 + " " + "¡¡Muchas gracias!!")
+
 }
-aviso("Precentarse en AV Cordoba 2677 de 9:00 hs a 15:00" )
+aviso("Precentarse en AV Cordoba 2677 de 9:00 hs a 15:00")
 
 
-
-
-
-=======
->>>>>>> 6f294e953e7263090a1030374c4f2154d6e70494
